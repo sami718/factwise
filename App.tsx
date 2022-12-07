4dab67464c0e315013552cd017874cbd72095dcf
samiksha jagtap <jagtapsamikha1@gmail.com>
2022-12-07 16:40:08 +0530

    fix: remove comment code

--- App.tsx
+++ App.tsx
@@ -20,7 +20,6 @@ const App = () => {
     isModalVisible: false,
     selectedCeleb: null,
   });
-  // Icon.loadFont().then();
 
   const deleteCelebrities = () => {
     let index = celebrities.findIndex(
